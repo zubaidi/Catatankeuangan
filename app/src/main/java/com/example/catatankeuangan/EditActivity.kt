e4dc2e7e877b8248f839fed3d19030b12765b0bb
zubaidi <muhammadzubaidi333@gmail.com>
2023-08-08 13:36:36 +0700

    tampil data

--- app/src/main/java/com/example/catatankeuangan/EditActivity.kt
+++ app/src/main/java/com/example/catatankeuangan/EditActivity.kt
@@ -3,11 +3,17 @@ package com.example.catatankeuangan
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.catatankeuangan.database.DataDB
 import com.example.catatankeuangan.databinding.ActivityEditBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class EditActivity : AppCompatActivity() {
 
     private lateinit var bindEdit: ActivityEditBinding
+    private val db by lazy { DataDB(this) }
+    private var dataId = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindEdit = ActivityEditBinding.inflate(layoutInflater)
@@ -15,6 +21,22 @@ class EditActivity : AppCompatActivity() {
         bindEdit.backButton.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
+        }
+        this.getDataKeuangan()
+    }
+
+    private fun getDataKeuangan() {
+        dataId = intent.getIntExtra("data_id", 0)
+        CoroutineScope(Dispatchers.IO).launch {
+            val datas = db.dataDAO().getAllDataByID(dataId)[0]
+            bindEdit.txtInputNominal.setText(datas.nominal.toString())
+            bindEdit.txtInputKeterangan.setText(datas.keterangan)
+            bindEdit.txtInputTanggal.setText(datas.tanggal)
+            if ( datas.kategori == "Masuk" ) {
+                bindEdit.rbMasuk.isChecked
+            } else {
+                bindEdit.rbKeluar.isChecked
+            }
         }
     }
 }
